d4d05a5f3e67ced42f6773b7000f7bd8b9e1ae69
ily-e <ily-e@yandex.ru>
2021-04-13 11:42:50 +0300

    First test

--- src/UniversalExcelReader.java
+++ src/UniversalExcelReader.java
@@ -1,5 +1,8 @@
 public class UniversalExcelReader {
     public static void main (String[] args) {
-        System.out.print("Rdf");
+        System.out.println("Rdf");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
